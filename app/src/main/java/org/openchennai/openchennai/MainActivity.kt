87e659249d798a72ab99f520994f1af27abc9f00
Anirudh S <anirudh@fundsindia.com>
2018-05-17 10:32:41 +0530

    API calls and response handling for positions and leaderboard

--- app/src/main/java/org/openchennai/openchennai/MainActivity.kt
+++ app/src/main/java/org/openchennai/openchennai/MainActivity.kt
@@ -12,6 +12,12 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -19,20 +25,63 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
 import org.openchennai.openchennai.menuFragment.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    class Reporter {
+        var name: String = "";
+        var link: String = "";
+        var count: Int = 0;
+        var rank: Int = 0;
+
+        constructor() {}
+
+        constructor(name: String, link: String, count: Int, rank: Int) {
+            this.name = name;
+            this.link = link;
+            this.count = count;
+            this.rank = rank;
+        }
+    }
 
     private val OPEN_CHENNAI_LINK = "https://openchennai.org"
     private val TWITTER_LINK = "https://twitter.com/openchennai"
     private val FACEBOOK_LINK = "https://facebook.com/openchennai"
     private val INSTAGRAM_LINK = "https://instagram.com/openchennai"
     private val YOUTUBE_LINK = "https://www.youtube.com/channel/UCZx7M37xyu0s6-7OJ2uverg"
+
     private val GITHUB_LINK = "https://github.com/openchennai"
 
     private lateinit var mMap: GoogleMap
 
     private lateinit var mapFragment: SupportMapFragment
+    var data: JSONArray = JSONArray()
+    var responsesReceived: Int = 0;
+
+    val unsortedReps = MutableList(0) { Reporter() }
+
+    var reporters = MutableList(0) { Reporter() }
+
+    val positions: MutableList<String> = ArrayList()
+
+    companion object {
+        @JvmStatic
+        val repositories = arrayOf(
+                "Roads",
+                "Water-and-Sanitation",
+                "Electricity",
+                "Garbage",
+                "Public-Transport",
+                "Traffic",
+                "Parks-and-Playgrounds",
+                "Trees"
+                // 'Community",
+                // 'openchennai.github.io'
+        );
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +101,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.placeHolder, fragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.github.com/repos/OpenChennai"
+
+
+        for (repo in repositories) {
+            val stringRequest = JsonArrayRequest(Request.Method.GET, url + "/" + repo + "/issues", null,
+                    Response.Listener<JSONArray> { data ->
+                        this.data = data;
+                        this.processData();
+                        this.responsesReceived++;
+                        this.checkAndPrepareTable();
+                        // Display the first 500 characters of the response string.
+//                    textView.text = "Response is: ${response.substring(0, 500)}"
+                    },
+                    Response.ErrorListener {
+                        //                    textView.text = "That didn't work!"
+                    }
+            )
+            queue.add(stringRequest)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -207,19 +281,78 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         fragmentTransaction.commit()
     }
 
-    companion object {
-        @JvmStatic
-        val repositories = arrayOf(
-                "Roads",
-                "Water-and-Sanitation",
-                "Electricity",
-                "Garbage",
-                "Public-Transport",
-                "Traffic",
-                "Parks-and-Playgrounds",
-                "Trees"
-                // 'Community",
-                // 'openchennai.github.io'
-        );
+
+    fun processData() {
+        for (i in 0..this.data.length() - 1) {
+            val content = this.data.getJSONObject(i)
+            val body: String = content.getString("body");
+            val lines = body.split("\r\n");
+            this.getLeaderboardDetails(lines);
+            this.getLocations(lines);
+        }
+
+    }
+
+    fun getLeaderboardDetails(lines: List<String>) {
+        var name = "";
+        var link = "";
+
+        if (lines[0].startsWith("Reporter: ")) {
+            val reporterArr = lines[0].split("Reporter: ")
+            name = reporterArr[1]
+        }
+
+        if (lines[1].startsWith("Link: ")) {
+            val linkArr = lines[1].split("Link: ");
+            link = linkArr[1];
+        }
+
+        if (name != "" && link != "") {
+            var userExists = false;
+            for (i in 0..this.unsortedReps.size - 1) {
+                if (this.unsortedReps[i].name == name && this.unsortedReps[i].link == link) {
+                    this.unsortedReps[i].count++;
+                    userExists = true;
+                    break;
+                }
+            }
+
+
+            if (!userExists) {
+                val reporter: Reporter = Reporter(name, link, 1, 0);
+                this.unsortedReps.add(reporter);
+            }
+        }
+    }
+
+    fun getLocations(lines: List<String>) {
+        for (i in 0..lines.size - 1) {
+            if (lines[i].startsWith("Location: ")) {
+                val extractedArray = lines[i].split("Location: ");
+                val latLong = extractedArray[1];
+                this.positions.add(latLong);
+            }
+        }
+    }
+
+    fun checkAndPrepareTable() {
+        if (this.responsesReceived == repositories.size) {
+            this.sortResults();
+        }
+    }
+
+    fun sortResults() {
+        val sortedReps: MutableList<Reporter> = this.unsortedReps
+        sortedReps.sortBy { -it.count }
+
+        var rank = 1;
+        for (i in 0..sortedReps.size - 1) {
+            if (i > 0 && sortedReps[i].count < sortedReps[i - 1].count) {
+                rank++;
+            }
+            sortedReps[i].rank = rank;
+        }
+
+        this.reporters = sortedReps;
     }
 }
